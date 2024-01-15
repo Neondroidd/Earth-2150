4c26d6d9783e1899989027331a390e8e8e0f75dc
Neondroidd <apakabarhai514@gmail.com>
2024-01-15 15:41:32 +0800

    p

--- src/public/main/index.js
+++ src/public/main/index.js
@@ -1,3 +1,36 @@
+//Quote
+async function fetchJson(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+}
+fetchJson("https://api.quotable.io/random")
+  .then((data) => {
+    displayContent(data.content);
+    displayAuthor(data.author);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
+
+function displayContent(content) {
+  const contentElement = document.getElementById("container-quotes");
+  contentElement.textContent = content;
+}
+
+function displayAuthor(author) {
+  const authorElement = document.getElementById("author-quotes");
+  authorElement.textContent = author;
+}
+
 // Get the h1 element
 var h1 = document.getElementById("Changer");
 
